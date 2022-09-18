edda8ff6d02947078051aa31694a8472a9b12339
bijokmathew <bijo.nbr@hotmail.com>
2022-09-18 06:47:08 +0000

    define run_game and added logic for validate the user name

--- run.py
+++ run.py
@@ -1,3 +1,58 @@
+from string import ascii_letters
+
+
+def validate_name(name):
+    """
+    * check name contain any number
+    * check name conian atleast 2 characters
+    * check name conain any special characters
+    * uses difference() method of python to validate
+    * returen True : name is valid
+              False : name is invalid
+
+    """
+    print(ascii_letters)
+    print(len(name))
+    if len(name) < 2:
+        print("correct")
+    if len(name) < 2 or set(name).difference(ascii_letters):
+        return False
+    else:
+        return True
+
+
+def run_game():
+    """
+    * All game's logic functions call from here
+    * Get the uer name and validate it
+    * if name is valid
+        get the orginal word and ask the user to guesss letter
+    * validate the guessed letter as per the rules
+    * if the guess letter is correct then display the gussed letter
+      in the orginal word like _ _ _ X_ _ _
+    * if user guess is wrong then start draw hangman
+    * display the user gussed letters and reduce the chance count
+    * if user guessed all letters correctly then user win
+    * if user fails to guess all letter then user lose and display
+      the hangman
+
+    """
+    while True:
+        print("Please enter your name\n")
+        print("Name should contain only letters and should not have any special characters")
+        print("Example: Deric \n\n")
+
+        player_name = input("Enter your name here:\n")
+        # validate the player name againts the rules
+        if validate_name(player_name):
+            print("Name is valid")
+            break
+        else:
+            print(
+                """Hmmm....this doesn't seem right \U0001F914 """
+                """ Please make sure to enter a name!"""
+            )
+
 
 
 def main():
@@ -8,7 +63,8 @@ def main():
      * Execute  user input validation 
     """
     while True:
-        print("+++++ Welcome to HangMan Game +++++ \n")
+        print("\n\n")
+        print("+++++ Welcome to HangMan Game +++++ ")
         print("\n\n")
 
         print("   1.   Play Game   ")
@@ -22,20 +78,16 @@ def main():
 
         if user_choice == 1:
             print("Congratzz!! You started your game \n")
-            # start the game 
+            # start the game
             run_game()
             break
         elif user_choice == 2:
-            #Game rules and hints to play 
+            # Game rules and hints to play
             game_help()
             break
         elif user_choice == 3:
             print("Your leaving the game. See you soon..")
             exit()
-
-
-
-
 
 
 main()
