41c444a142e6708679d776e6bba8e15c14ff2a4b
bijokmathew <bijo.nbr@hotmail.com>
2022-09-18 13:33:50 +0000

    Added code for checking guess letter present in the word

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import random
 from string import ascii_letters
+from words import list_of_words
 
 
 def validate_name(name):
@@ -52,7 +54,52 @@ def run_game():
                 """Hmmm....this doesn't seem right \U0001F914 """
                 """ Please make sure to enter a valid name!"""
             )
+    # get randomly actual word from list of words
+    actual_word = random.choice(list_of_words)
+
+    # define the number of guess and it is equal to the length of the actual word
+    number_of_guess = len(actual_word)
+    # store all the user gussed letters for validating user repeating the same letter
+    user_guessed_letters = ""
+    # store user guess letter
+    user_guess_letter = ""
+
+    # repeat the user guess until the number of life become 0 
+    while (number_of_guess > 0):
+        # To display gussed letter in the original word like _ _ X _ _
+        display_guss_letter = ""
+        # if user guessed letter present in actual word then add guessed 
+        # letter else add "_" to the display word
+        display_guss_letter = [letter if letter in user_guess_letter else "_" for letter in actual_word]
+        print(" ".join(display_guss_letter))
+
+        # Display the number of guess left and all guessed letters
+        print(f"Number of guess left : {number_of_guess}\n")
+        print(f"Gussed letter: {user_guessed_letters}")
+        print("\n\n")
+        print(f"Actual word:  {' '.join(display_guss_letter)}")
+
+        # Ask user to guess the letter
+        user_guess_letter = input("Guess a letter here : ")
+        # validate the user guessed letter
+        if (user_guess_letter in ascii_letters):
+            user_guessed_letters += user_guess_letter
+        else:
+            print("Please enter valid character \n")
+            user_guess_letter = input("Guess a letter here : ")
+        # if guessed letter not present in present in actual word then
+        # decrement the number of guess and draw hangman
+        if user_guess_letter not in actual_word:
+            number_of_guess -= 1
+            draw_hangman(number_of_guess)
+        else:
+            print("Your guess is correct !!!  {user_guess_letter} found ")
+        
+
+
+        
     
+
 
 def main():
     """
