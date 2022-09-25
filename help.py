80c46b88ad186f87c242cd573bfe4e501c1af4e2
bijokmathew <bijo.nbr@hotmail.com>
2022-09-25 21:45:15 +0000

    Align the code and look and feel of help.py

--- help.py
+++ help.py
@@ -6,51 +6,50 @@ def get_game_help():
     * Define the how to play the game
     * Explain details of the game like invalid entry, error cases
     """
-    print(colored("-" * 80, 'blue'))
-    print('\n')
     print(colored(" " * 30 + "How to play the GAME", 'yellow'))
+    print(colored("-" * 80, 'blue'))
     rule_list = (
-        '\n1: Please enter valid options like 1,2,3 and 4 to play the game'
+        '\n 1: Please enter valid options like 1,2,3 and 4 to play the game'
         '\n'
-        '\n#: If user select option 1 then below are the steps to play the'
-        'game'
+        '\n #: If user select option 1 then below are the steps to play the'
+        ' game'
         '\n'
-        '\n2: Enter valid name i.e name contains only alphabet and'
-        'minumum 2 letter'
+        '\n 2: Enter valid name i.e name contains only alphabet and'
+        ' minumum 2 letter'
         '\n'
-        '\n3: Guess only single alphabet letter and number of life'
-        'is equal to the length of the actual word'
+        '\n 3: Guess only single alphabet letter and number of life'
+        ' is equal to the length of the actual word'
         '\n'
-        '\n4: If guessed letter is not found in the actual word then you'
-        'loose one number of life and display the hangman based on'
-        'the number of life remaining'
+        '\n 4: If guessed letter is not found in the actual word then you'
+        ' loose one number of life and display the hangman based on'
+        ' the number of life remaining'
         '\n'
-        '\n5: If guessed letter is already gussed previously then try to'
-        'guess another letter and you wont loose the number of life'
+        '\n 5: If guessed letter is already gussed previously then try to'
+        ' guess another letter and you wont loose the number of life'
         '\n'
-        '\n6: If guessed letter and actual word are same then you'
-        'won the game'
+        '\n 6: If guessed letter and actual word are same then you'
+        ' won the game'
         '\n'
-        '\n7: Check the current number of life used is fit in top five in'
-        'the google sheet and then added the player name and number of life'
-        'used to the google sheet'
+        '\n 7: Check the current number of life used is fit in top five in'
+        ' the google sheet and then added the player name and number of life'
+        ' used to the google sheet'
         '\n'
-        '\n8: If guessed letter and actual word are same then you'
-        'won the game'
+        '\n 8: If guessed letter and actual word are same then you'
+        ' won the game'
         '\n'
-        '\n9: If number of life is zero and guessed letter and actual word'
-        'are not same then you loose the game'
+        '\n 9: If number of life is zero and guessed letter and actual word'
+        ' are not same then you loose the game'
         '\n'
-        '\n#: If user select option 2, it shows the game details'
+        '\n #: If user select option 2, it shows the game details'
         '\n'
-        '\n#: If user select option 3, it shows the top five list of'
-        'players details'
+        '\n #: If user select option 3, it shows the top five list of'
+        ' players details'
         '\n'
-        '\n#: If user select option 4, it exit the game')
+        '\n #: If user select option 4, it exit the game')
 
     print(colored(rule_list, 'cyan'))
 
-    print("\n\n", colored("-" * 80, 'blue'))
+    print("\n", colored("-" * 80, 'blue'))
     print('\n')
 
     print(" " * 24 + colored("\n Please enter", 'blue', attrs=['bold']),
@@ -58,4 +57,7 @@ def get_game_help():
           colored("or", 'blue', attrs=['bold']),
           colored(" Enter", 'white', attrs=['bold']),
           colored("to return to main menu...\n", 'blue', attrs=['bold']))
-    input("\n ")
+    try:
+        input("\n ")
+    except BaseException:
+        pass
